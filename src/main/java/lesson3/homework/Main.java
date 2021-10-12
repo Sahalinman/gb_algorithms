cd35f20b5fd44d65f51c6040faf2b9cc9daca950
Sahalinman <taras_off@bk.ru>
2021-10-12 21:20:50 +0300

    # lesson 3 - Доработка, подготовка к сдаче

--- src/main/java/lesson3/homework/Main.java
+++ src/main/java/lesson3/homework/Main.java
@@ -9,12 +9,43 @@ public class Main {
     public static void main(String[] args) {
 
         // Задание 1
+        /** Вывод
+         * Пропущено число : 11
+         */
          Task1();
 
         // Задание 2
+        /** Вывод
+         * add element to right side : true
+         * add element to left side : true
+         * add element to right side : true
+         * add element to left side : true
+         * add element to right side : true
+         * add element to left side : true
+         * add element to left side : true
+         * [7, 6, 5, 4, 3, 2, 1]
+         * remove from left side : 7
+         * [6, 5, 4, 3, 2, 1]
+         * remove from right side : 1
+         * [6, 5, 4, 3, 2]
+         */
          Task2();
 
         // Задание 3
+        /** Вывод
+         * add element: true == > [34]
+         * add element: true == > [34, 12]
+         * add element: true == > [34, 12, 20]
+         * add element: true == > [34, 12, 20, 16]
+         * add element: true == > [34, 12, 20, 16, 14]
+         * Очередь полная? : true
+         * remove value from head : == > [12, 20, 16, 14]
+         * remove value from head : == > [20, 16, 14]
+         * remove value from head : == > [16, 14]
+         * remove value from head : == > [14]
+         * remove value from head : == > []
+         * Очередь пустая? : true
+         */
          Task3();
     }
 
